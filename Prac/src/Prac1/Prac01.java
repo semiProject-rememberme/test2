126549a81cdc8a7a8a24bf9d8fd41e082656d5a2
user1 <user1@DESKTOP-2LFI64H>
2020-04-24 14:51:44 +0900

    조원2

--- Prac/src/Prac1/Prac01.java
+++ Prac/src/Prac1/Prac01.java
@@ -5,6 +5,8 @@ public class Prac01 {
 	public static void main(String[] args) {
 		System.out.println("안녕? 나는 조장이야");
 		System.out.println("안녕? 나는 조원이야");
+		
+		System.out.println("조원 두번째");
 	}
 
 }
